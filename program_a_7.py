9b48085fd515fa1e7e2a5d3a7aecb971e7d89cdf
wiltis <110127597+wiltis09@users.noreply.github.com>
2025-09-26 21:03:02 +0200

    ffas

---
+++ program_a_7.py
@@ -0,0 +1,8 @@
+
+print(" ")
+print(" ")
+print(" ")
+print("pappa kallar \ för ", "\"bakåtsnedstreck\"", "men jag säger", " \"backslash\"")
+print(" ")
+print(" ")
+print(" ")
---
+++ program_b_7 .py
@@ -0,0 +1,8 @@
+
+print(" ")
+print(" ")
+print(" ")
+print("\"Whats\`s up\"", "said Ted.")
+print(" ")
+print(" ")
+print(" ")
