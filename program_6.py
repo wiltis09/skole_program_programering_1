be436285f90f108353bbb9b6cb0f109a2c687094
wiltis <110127597+wiltis09@users.noreply.github.com>
2025-10-01 14:44:26 +0200

    ffffffffff

--- program_6.py
+++ program_6.py
@@ -1,4 +1,9 @@
-print("Input two numbers:")
+
+import math
+import sys 
+
+
+print("Input thire numbers:")
 
 def input_number(prompt):
     while True:
@@ -8,43 +13,38 @@ def input_number(prompt):
         except:
             print("Please enter a valid integer.")
 
-input_num_1 = input_number("First number: ")
-input_num_2 = input_number("andra number: ")
-input_num_3 = input_number("tredj number: ")
+input_num_1 = 1 #input_number("First number: ") 
+input_num_2 = 1 #input_number("Second number: ")
+input_num_3 = input_number("Third number: ")
 
+
+print ((len(str(input_num_1)) - (len(str(input_num_1)) - 1)))
+def print_num1(num1, num2, num3):
+    width = 4 #len(str(num))
+    inputs = [num1,num2,num3]
+    s1 = str(num1 + num2 + num3)
+    for num in inputs:
+        if len(str(num)) > 3:
+            width = width + (len(str(num)) - 2)
+    for num in inputs:
+        s = str(num)
+        if num == inputs[2]:
+            print( "+  " + s.rjust(width - 1))
+        else:
+            if len(str(num)) > 3:
+                print( "  " + s.rjust(width))
+            else:
+                print("  " + s.rjust(width))
+    spacer = "" 
+    for l in range(0, width):
+        spacer = str(spacer) + "="
+    print(spacer + "===")
+    print("  " + s1.rjust(width))
 variabel_summa = input_num_1 + input_num_2 + input_num_3
 print("Tack för att du använde programmet!")
 print("")
 print("")
 print("")
-if input_num_1 < 10:
-    print("    ", input_num_1)
-elif input_num_1 < 100:
-    print("   ", input_num_1)
-elif input_num_1 < 1000:
-    print("  ", input_num_1)
 
-if input_num_2 < 10:
-    print("    ", input_num_2)
-elif input_num_2 < 100:
-    print("   ", input_num_2)
-elif input_num_2 < 1000:
-    print("  ", input_num_2)
 
-if input_num_3 < 10:
-    print("+   ", input_num_3)
-elif input_num_3 < 100:
-    print("+  ", input_num_3)
-elif input_num_3 < 1000:
-    print("+ ", input_num_3)
-
-print("======")
-if variabel_summa < 10:
-    print("    ", variabel_summa)
-elif variabel_summa < 100:
-    print("   ", variabel_summa)
-elif variabel_summa < 1000:
-    print("  ", variabel_summa)
-print("")
-print("")
-print("")
+print(print_num1(input_num_1, input_num_2, input_num_3))
