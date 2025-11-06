e21b2acadfca2d7de24564dce1a8f6969b929dad
wiltis09 <110127597+wiltis09@users.noreply.github.com>
2025-11-06 08:49:40 +0100

    klass 5

--- Programmeringsövningar till kapitel 5/program_7.py
+++ Programmeringsövningar till kapitel 5/program_7.py
@@ -1,28 +1,6 @@
-
-def input_number(prompt):
-    while True:
-        try:
-            value = int(input(prompt))
-            return value
-        except:
-            print("Please enter a valid integer.")
-
-while True:
-    ålder = input_number("Hur gammal är du:")
-    if ålder < 18:
-        print("tyvärr kan vi inte bevilja fakturabetalning")
-        break
-    else:
-        årsinkomsten = input_number("Hur mycket är din årliga inkomst:")
-        if årsinkomsten < 120000:
-            print("tyvärr kan vi inte bevilja fakturabetalning")
-            break
-        else:
-            kreditanmärkningar = input_number("Hur många kreditanmärkningar har du?:")
-            if kreditanmärkningar > 0:
-                print("tyvärr kan vi inte bevilja fakturabetalningen")
-                break
-            else:
-                print("Fakturabetalning beviljad")
-
-print("Tack för att du använde programmet!")
+x=0
+for i in range(10):
+    x = x + 1
+for j in range(10):
+    x = x + 1
+print(x)
