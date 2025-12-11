15fa9ab47ff5a198c1a8e998f1e81ff8f14c6302
wiltis09 <110127597+wiltis09@users.noreply.github.com>
2025-12-11 09:35:30 +0100

    Lägg till konvertering från Fahrenheit till Celsius
    Implementerade ett enkelt program som tar emot temperatur i Fahrenheit,
    räknar om den till Celsius och skriver ut resultatet. Lade även in tomma
    rader före och efter utskriften för att göra terminalutdata mer lättläst.

---
+++ Turtel/c till f.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    print("")
+    
+fahr = int(input("Mata in tempreturen i farenheit: "))
+cels = int(5 * (fahr - 32) / 9)
+print("din temperatur i celsius är", cels)
+
+for i in range(5):
+    print("")
